78eefb848073458fd57e58b3d0b96c164adb81e1
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-10-10 21:57:20 +0300

    create company and store models in core app and cutomoize them in admin page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
-# Register your models here.
+from .models import Company,Store
+class CompanyAdmin(admin.ModelAdmin):
+    list_display=['id','name']
+    fields=['name']
+class StoreAdmin(admin.ModelAdmin):
+    list_display=['store_number','company']
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Store,StoreAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+
 from django.db import models
 
-# Create your models here.
+class Company(models.Model):
+    name=models.CharField(max_length=200)
+    def __str__(self) -> str:
+        return self.name
+class Store(models.Model):
+    company=models.ForeignKey(Company,on_delete=models.CASCADE,null=True,blank=True)
+    store_number=models.CharField(max_length=200)
+    address=models.TextField()
