38e054863bdbc9be2e4ada466911a955ce918ffa
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-10-10 22:43:49 +0300

    delete inventory app

--- inventory/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from inventory.models import Customer,Vihicle
-
-admin.site.register(Customer)
-admin.site.register(Vihicle)
--- inventory/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.class Customer(models.Model):
-class Customer(models.Model):
-    name=models.CharField(max_length=200)
-class Vihicle(models.Model):
-    name=models.CharField(max_length=200)
-    customers=models.ManyToManyField(Customer)
-    price=models.IntegerField()
