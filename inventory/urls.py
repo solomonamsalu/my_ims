33027840cfea2fecf03a16453e7f51f5174953a5
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-11-30 19:50:22 +0300

    item view detail view done

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns=[
+    # path('item_create',views.ItemCreate.as_view()),
     path('items/',views.ItemList.as_view()),
     path('<int:pk>/item_detail',views.ItemDetail.as_view()),
+    path('index/',views.index)
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse,HttpRequest
 from .models import Item,Supplier
 from django.views import generic
+from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
+
+def index(request):
+    item=Item.objects.all()
+    return HttpResponse(item)
+
 class ItemList(generic.ListView):
     model=Item    
 class ItemDetail(generic.DetailView):
     model=Item
 
+    
