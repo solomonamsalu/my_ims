3eeb0d53d8de54e42eca8e8e3a18da1bbae9a086
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-12-03 21:56:15 +0300

    add style to inventory/forms

--- inventory/forms.py
+++ inventory/forms.py
@@ -1,11 +1,34 @@
 from django import forms
-# from django.forms import ModelForm
+from django.forms import ModelForm, TextInput, EmailInput
 from .models import Item
 class AddItemForm(forms.ModelForm):
    class Meta:
         model=Item
         fields = "__all__"
-        lables={"name":"item_name"}
+        widgets = {
+        'name': TextInput(attrs={
+            'class': "form-control"
+            }),
+        'sku_number':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'selling_price':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'cost_price':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'max_stock':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'onhand_stock':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'reorder_point':TextInput(attrs={
+            'class': "form-control"
+            }),
+        'priferred_supplier':TextInput(attrs={
+            'class': "form-control"
+            })
+            }
 
-    # def save(self, commit):
-    #     return super().save(commit)
