370f83f5486aa43031a05f7fa6f9712cb76aa4ac
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-12-07 09:12:36 +0300

    add login and logout pages

--- my_ims/urls.py
+++ my_ims/urls.py
@@ -19,4 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/',include('inventory.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
