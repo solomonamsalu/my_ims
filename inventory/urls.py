640b15ea755feab9e2649dd6c9ec33695ea98d7c
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-12-07 10:20:40 +0300

    add authontication requerment to itemlist and itemdetail views

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns=[
     # path('item_create',views.ItemCreate.as_view()),
     # url(r'form', views.AddItemForm, name='form'),
-    path('items/',views.ItemList.as_view(),name="items"),
+    path('',views.ItemList.as_view(),name="items"),
     path('<int:pk>/item_detail',views.ItemDetail.as_view()),
     path('index/',views.index),
     path('item_create/',views.itemcreate)
--- inventory/views.py
+++ inventory/views.py
@@ -4,6 +4,9 @@ from .models import Item,Supplier
 from django.views import generic
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
 from .forms import AddItemForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 def index(request):
     item=Item.objects.all()
@@ -16,9 +19,10 @@ def itemcreate(request):
     else:
         form=AddItemForm
     return render(request,'inventory/item_create.html',{'form':form})
+
+@method_decorator(login_required, name='dispatch')
 class ItemList(generic.ListView):
     model=Item    
 class ItemDetail(generic.DetailView):
     model=Item
 
-    
