f4e36f8b51343b74e73946418995b98ba2754fdc
Solomon Amsalu <amsalusolomon12@gmail.com>
2022-12-16 22:08:15 +0300

    add css and add buttons to all pages

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns=[
  path('companies/',views.CompanyList.as_view(),name='company_list'),
- path('<int:pk>/company_detail',views.CompanyDetail.as_view(),name='company-detail'),
+ path('<int:pk>/company_detail',views.CompanyDetail.as_view(),name='company_detail'),
  path('company_create/',views.CompanyCreate.as_view(),name='company_create'),
  path('<int:pk>/company_update/',views.CompanyUpdate.as_view(),name='company_update'),
  path('<int:pk>/company_delete/',views.CompanyDelete.as_view(),name='company_delete'),
